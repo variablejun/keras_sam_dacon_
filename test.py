19a7fc6cd8a9187502bfb457c32b9e21887d5be2
variablejun <crescendo0217@gmail.com>
2021-09-13 23:56:09 +0900

    xx

--- test.py
+++ test.py
@@ -101,8 +101,11 @@ def create_deep_learning_model():
     model = Sequential()
     model.add(Dense(2048, input_dim=2048, kernel_initializer='random_uniform', activation='softplus'))
     model.add(Dense(1024, activation='softplus'))
-    model.add(Dense(512, activation='softplus'))
-    model.add(Dense(512, activation='softplus'))
+    model.add(Dense(1024, activation='softplus'))
+    model.add(Dense(1024, activation='softplus'))
+    model.add(Dense(1024, activation='softplus'))
+    model.add(Dense(1024, activation='softplus'))
+
     
     
     model.add(Dense(100, activation='softplus'))
